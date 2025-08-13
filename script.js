f9a0d69a0a55555b0e16c8d909f3a9691b874886
AbdullahMedhat141 <134795680+AbdullahMedhat141@users.noreply.github.com>
2025-08-13 11:12:13 +0300

    Added the main functionality: Adding items to the list

---
+++ script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const form = document.querySelector(".todo-form");
+const btnAdd = document.querySelector(".btn-add");
+const inputAdd = document.querySelector(".input-add");
+const list = document.querySelector(".list");
+
+function handleAdd() {
+  const text = inputAdd.value.trim();
+  if (!text) return;
+  const task = document.createElement("li");
+  task.classList.add("task");
+  const taskCheckBox = document.createElement("input");
+  taskCheckBox.setAttribute("type", "checkbox");
+  const taskP = document.createElement("p");
+  taskP.textContent = text;
+  const taskDeleteBtn = document.createElement("button");
+  taskDeleteBtn.textContent = "❌";
+  taskDeleteBtn.classList.add("delete-btn");
+  task.append(taskCheckBox, taskP, taskDeleteBtn);
+  list.append(task);
+  inputAdd.value = "";
+  inputAdd.focus();
+}
+
+inputAdd.addEventListener("input", (e) => {
+  text = e.target.value;
+});
+
+btnAdd.addEventListener("click", handleAdd);
+inputAdd.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    handleAdd();
+  }
+});
